3e70713271c6c70a69dbb22102c733b2bfa2bba6
asapriyank@gmail.com <asapriyank@gmail.com>
2019-12-16 01:14:54 +0530

    API gateway

---
+++ netflix-zuul-api-gatway-server/src/main/java/com/ex/netflixzuulapigatwayserver/ZuulLoggingFilter.java
@@ -0,0 +1,44 @@
+package com.ex.netflixzuulapigatwayserver;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public Object run() throws ZuulException {
+		// TODO Auto-generated method stub
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		logger.info("request->>{} request uri -> {}", request, request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+}
